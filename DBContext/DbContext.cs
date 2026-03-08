dade788f08dc77cd0f670d3ef029ea265de227cb
Shameer Navas <shameernavasofficial@gmail.com>
2026-03-09 02:12:20 +0300

    Add files via upload

---
+++ DBContext/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RECIPE.Models;
+
+namespace RECIPE.DBContext
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
